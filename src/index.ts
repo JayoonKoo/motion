85675a32eb5c7b40d1255ff7419e3d4ab56acc58
JayoonKoo <rnwkdbs12@gmail.com>
2021-11-16 02:42:03 +0900

    feat: 이벤트 등록 클래스 생성

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-console.log("hello World");
+// Error State
+type EventAddErrorState = {
+  result: "fail";
+  reason: "noSelector";
+};
+
+type SuccessState = {
+  result: "success";
+};
+
+type ResultState = EventAddErrorState | SuccessState;
+
+// HandleEvent
+type EventType = {
+  selector: string;
+  eventName: keyof ElementEventMap;
+};
+
+type Cb = (this: Element, ev: Event) => void;
+
+interface HandleEvent {
+  addEvent(eventInfo: EventType, cb: Cb): ResultState;
+}
+
+class HandleEventImpl implements HandleEvent {
+  private events: EventType[] = [];
+
+  addEvent = (eventInfo: EventType, cb: Cb): ResultState => {
+    const { selector, eventName } = eventInfo;
+    const el = document.querySelector(selector);
+    if (el === null) {
+      return {
+        result: "fail",
+        reason: "noSelector",
+      };
+    }
+    el.addEventListener(eventName, cb);
+    this.events.push(eventInfo);
+    return {
+      result: "success",
+    };
+  };
+}
+
+// MakeElement
+interface MakeElement {}
+class MakeElementImpl implements MakeElement {}
+
+// AddDom
+interface AddDom {}
+
+class AddDomImpl implements AddDom {}
